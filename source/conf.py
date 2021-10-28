180dbd5af216924af547e369a673bda357933b43
Ray <xiamenglei321@163.com>
2021-10-28 16:49:33 +0800

    2021-10-28

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-os.system("pip install build_sphinx")
+os.system("python setup.py build_sphinx")
 
 
 # -- Project information -----------------------------------------------------
